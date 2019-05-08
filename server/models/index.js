874a60ceffb92eb66f412bbca77a4af181bd1f49
Juan Barros <juanlyrabarros@gmail.com>
2019-05-08 12:05:06 -0300

    Remove código desnecessário

--- server/models/index.js
+++ server/models/index.js
@@ -2,24 +2,12 @@ const Sequelize = require("sequelize");
 const logger = require("heroku-logger");
 
 // Models
-const ProposicaoModel = "./postgres/proposicao.js";
-const PerguntaModel = "./postgres/pergunta.js";
-const CandidatoModel = "./postgres/candidato.js";
-const TemaModel = "./postgres/tema.js";
-const VotacaoModel = "./postgres/votacao.js";
-const VotacaoUModel = "./postgres/votacaou.js";
-const RespostaModel = "./postgres/resposta.js";
-const RespostaUModel = "./postgres/respostasU.js";
 const UsuarioModel = "./postgres/usuario.js";
-const TemasUModel = "./postgres/temasu.js";
-const ComissoesModel = "./postgres/comissoes.js";
-const ComposicaoComissoesModel = "./postgres/composicao-comissoes.js";
 
 if (!global.hasOwnProperty("models")) {
   const db = require("../config/keys").postgresURI;
-
   // Connect to Postgres
-  const sequelize = new Sequelize(db, {
+  const sequelize = new Sequelize({
     host: "localhost",
     dialect: "postgres",
     operatorsAliases: false,
@@ -37,19 +25,7 @@ if (!global.hasOwnProperty("models")) {
   global.models = {
     Sequelize: Sequelize,
     sequelize: sequelize,
-    tema: sequelize.import(TemaModel),
-    usuario: sequelize.import(UsuarioModel),
-    candidato: sequelize.import(CandidatoModel),
-    pergunta: sequelize.import(PerguntaModel),
-    proposicao: sequelize.import(ProposicaoModel),
-    resposta: sequelize.import(RespostaModel),
-    votacao: sequelize.import(VotacaoModel),
-    respostau: sequelize.import(RespostaUModel),
-    votacaou: sequelize.import(VotacaoUModel),
-    temasu: sequelize.import(TemasUModel),
-    comissoes: sequelize.import(ComissoesModel),
-    composicaoComissoes: sequelize.import(ComposicaoComissoesModel),
-    // add your other models here
+    usuario: sequelize.import(UsuarioModel)
   };
 
   Object.keys(global.models).forEach(modelName => {
