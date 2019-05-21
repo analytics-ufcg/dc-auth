7a1346b84d28290d7104ac3894042d3c034ec459
Juan Barros <juanlyrabarros@gmail.com>
2019-05-21 16:59:59 -0300

    Corrige redirectUri em server

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -40,6 +40,7 @@ router.post(
     if (!req.user) {
       return res.send(401, "User Not Authenticated");
     }
+    console.log(req.user)
     req.auth = {
       id: req.user.provider_id,
       firstName: req.user.first_name,
@@ -104,7 +105,7 @@ router.post("/googleCode", (req, res, next) => {
     code: req.body.code,
     client_id: req.body.clientId,
     client_secret: keys.googleAppSecret,
-    redirect_uri: 'http://localhost:8080',
+    redirect_uri: req.body.redirectUri + "/",
     grant_type: 'authorization_code'
   };
   
@@ -178,8 +179,7 @@ router.post("/facebookCode", (req, res, next) => {
 
   request.get({url: accessTokenUrl, qs: params}, (err, response, token) => {
     if (err) console.log(err); 
-    let accessToken = JSON.parse(token).access_token;   
-        
+    let accessToken = JSON.parse(token).access_token;      
     const peopleApiUrl = "https://graph.facebook.com/me?fields=id,name,email,first_name,picture{url}&access_token=" + accessToken 
     
     request.get({url: peopleApiUrl}, (err, response, profileFacebook) => {
@@ -192,7 +192,8 @@ router.post("/facebookCode", (req, res, next) => {
           provider: "facebook",
           provider_id: profile.id
         }
-      }).then((user, err) => {        
+      }).then((user, err) => {      
+         
         if (!user){
           const newUser = new Usuario({
             first_name: profile.first_name,
