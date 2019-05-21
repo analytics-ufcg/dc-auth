4cdda0b6a0170e9fac5c3b3efe68acc06d69df9a
marcellasiqueira <marcella.almeida@ccc.ufcg.edu.br>
2019-05-21 14:52:30 -0300

    melhora aparência do login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="login">
-    <img src="@/assets/dadocapital.png"/>
+    <img src="@/assets/dadocapital.png">
     <div class="header">Identifique-se</div>
-    <div
-      class="text"
-    >Ao entrar na Dado Capital, suas preferências de temas ficarão salvas para que você possa ver notificações específicas sobre os temas escolhidos. Não guardaremos nenhuma informação sensível, apenas dados públicos (nome, e-mail e foto).</div>
+
     <div class="social-buttons">
-      <button class="btn googleBtn" @click="login('google')"> <i class="fab fa-google"></i> Entrar com Google</button>
-      <button class="btn facebookBtn" @click="login('facebook')"> <i class="fab fa-facebook-square"></i> Entrar com Facebook</button>
+      <button class="btn googleBtn" @click="login('google')">
+        <i class="fab fa-google"></i> Entrar com Google
+      </button>
+      <button class="btn facebookBtn" @click="login('facebook')">
+        <i class="fab fa-facebook-square"></i> Entrar com Facebook
+      </button>
+    </div>
+
+    <span class="icon-login icon-lg"></span>
+    <div class="text">
+      <strong class="text-strong">Próximo login</strong>
+      <p>Ao entrar na Dado Capital, suas preferências de temas ficarão salvas para que você possa ver notificações específicas sobre os temas escolhidos.</p>
+    </div>
+
+    <span class="icon-seguranca icon-lg"></span>
+    <div class="text">
+      <strong class="text-strong">Privacidade</strong>
+      <p>Não guardaremos nenhuma informação sensível, apenas dados públicos (nome, e-mail e foto).</p>
     </div>
   </div>
 </template>
@@ -24,11 +38,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/styles/icons.scss";
+.icon-lg {
+  font-size: 4rem;
+  margin: 1.5rem 0rem 0rem 0rem;
+}
 .login {
   display: flex;
   align-items: center;
-  margin: 0vw 10vw;
   flex-direction: column;
+  @media screen and (max-width: 480px) {
+    margin: 0vw 10vw;
+  }
+  @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+    margin: 0vw 12vw;
+  }
+  @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
+    margin: 0vw 19vw;
+  }
+  margin: 0vw 26vw;
 }
 .social-buttons {
   text-align: center;
@@ -43,9 +71,9 @@ export default {
 }
 .text {
   @media screen and (max-width: 480px) {
-    margin: 0.5rem 1rem 2rem 1rem;
+    margin: 0.5rem 1rem 0rem 1rem;
   }
-  margin: 0.5rem 10rem 2rem 10rem;
+  margin: 0.5rem 10rem 0rem 10rem;
 }
 .btn {
   @media screen and (max-width: 480px) {
@@ -68,8 +96,10 @@ export default {
   opacity: 0.8;
 }
 .googleBtn {
+  margin: 1.5rem 0rem 0rem 0rem;
   color: #29303b;
   background-color: #fff;
   border: 1px solid #dedfe0;
 }
 </style>
+
